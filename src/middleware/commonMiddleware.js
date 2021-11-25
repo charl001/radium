2d620f165e438fd93b87084f3fa2cf5420837006
charl001 <37046640+charl001@users.noreply.github.com>
2021-11-25 16:46:53 +0530

    Update commonMiddleware.js

--- src/middleware/commonMiddleware.js
+++ src/middleware/commonMiddleware.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken')
 const UserModel = require("../models/usermodel")
-const Mid1=  async function(req, res, next)
-{
+const Mid1 = async function (req, res, next) {
 
     let token = req.headers['x-auth-token']
     if (!token) {
@@ -9,19 +8,24 @@ const Mid1=  async function(req, res, next)
     } else {
         let decodedtoken = jwt.verify(token, 'key')
         if (decodedtoken) {
-            let userdetails =  await UserModel.findOne({ _id: req.params.userId, isDeleted: false })
+            let userdetails = await UserModel.findOne({ _id: req.params.userId, isDeleted: false })
+            if (req.params.userId === decodedtoken._id) {
 
-            if (userdetails) {
-                req.isUserDetails=userdetails
-                next()
+                if (userdetails) {
+                    req.isUserDetails = userdetails
+                    next()
 
-            }else{
-                res.send({ status: false, message: 'User not found' })
+                } else {
+                    res.status(400).send({ status: false, message: 'Userid and path user id is not same' })
+                }
+            } else {
+                res.status(400).send({ status: false, message: 'User not found' })
             }
-        }else{
-            res.send( {status: false, message: 'Token not valid' })
+        } else {
+            res.status(400).send({ status: false, message: 'Token not valid' })
         }
+
     }
 }
 
-module.exports.Mid1=Mid1
+module.exports.Mid1 = Mid1
